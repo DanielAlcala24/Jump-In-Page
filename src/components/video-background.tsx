0ec9d828ed0554a804bc375f5a209c8272b68b60
Dany AV <danielav24102001@gmail.com>
2025-08-20 18:17:50 +0000

    Tienes razón este es el video de un sitio externo: https://videos.pexels

--- src/components/video-background.tsx
+++ src/components/video-background.tsx
@@ -3,7 +3,7 @@ export default function VideoBackground() {
     <div className="fixed inset-0 -z-10 h-full w-full overflow-hidden">
       <div className="absolute inset-0 bg-black/60" />
       <video
-        src="https://videos.pexels.com/video-files/8290333/8290333-hd_1920_1080_25fps.mp4"
+        src="https://videos.pexels.com/video-files/33536871/14260757_1920_1080_30fps.mp4"
         autoPlay
         loop
         muted
