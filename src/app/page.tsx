0bac1e51c74c6089b04aeec8ec6d970395bab0a9
Dany AV <danielav24102001@gmail.com>
2025-08-22 17:12:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="compr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from '@/components/header';
 import Eventos from '@/components/sections/eventos';
 import Experiencia from '@/components/sections/experiencia';
-import HazTuCompra from '@/components/sections/haz-tu-compra';
 import Inicio from '@/components/sections/inicio';
 import SocialIcons from '@/components/social-icons';
 import VideoBackground from '@/components/video-background';
@@ -14,7 +13,6 @@ export default function Home() {
       <SocialIcons />
       <main className="flex-1">
         <Inicio />
-        <HazTuCompra />
         <Experiencia />
         <Eventos />
       </main>
