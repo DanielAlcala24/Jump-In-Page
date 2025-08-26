8d5b902ab3b0bca97247c4313417e181606eb28e
Dany AV <danielav24102001@gmail.com>
2025-08-26 17:04:05 +0000

    no funciona mejor desactiva la animación

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,15 +78,10 @@ export default {
             height: '0',
           },
         },
-        'pulse-scale': {
-          '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(1.1)' },
-        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-scale': 'pulse-scale 8s infinite ease-in-out',
       },
     },
   },
