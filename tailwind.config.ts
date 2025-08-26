c7ad007b3108c3471d9da4079fd562eb233c5dab
Dany AV <danielav24102001@gmail.com>
2025-08-26 17:00:31 +0000

    va muy rapido la animación que sea mas lenta

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,7 +86,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-scale': 'pulse-scale 2s infinite ease-in-out',
+        'pulse-scale': 'pulse-scale 3s infinite ease-in-out',
       },
     },
   },
