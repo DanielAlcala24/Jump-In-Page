17d5bef36db23fc08baddea3b8d0e35b504c6260
Dany AV <danielav24102001@gmail.com>
2025-08-28 21:47:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <section id="inici

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import WhatsappButton from '@/components/whatsapp-button';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen">
       <VideoBackground />
       <Header />
       <SocialIcons />
