96628a13b3c68c8c8277fedb3554e1fb2a31ac88
Dany AV <danielav24102001@gmail.com>
2025-08-29 16:57:21 +0000

    el titulo del sitio web dice JumpZone, cambialo a Jump-In

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'JumpZone',
+  title: 'Jump-In',
   description: 'The ultimate trampoline park experience.',
 };
 
