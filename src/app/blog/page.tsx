88725daa81078f48802c7d3adc701d27815a76c8
Dany AV <danielav24102001@gmail.com>
2025-08-29 21:17:21 +0000

    en la pagina de blog agrega todo lo esencial, el logo, barra de navegaci

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,16 @@
 import BlogPosts from '@/components/sections/blog-posts';
 import VideoBackground from '@/components/video-background';
+import Header from '@/components/header';
+import SocialIcons from '@/components/social-icons';
+import WhatsappButton from '@/components/whatsapp-button';
+import Footer from '@/components/footer';
 
 export default function BlogPage() {
   return (
     <div className="flex flex-col min-h-screen">
       <VideoBackground />
+      <Header />
+      <SocialIcons />
       <main className="flex-1 pt-24">
         <section className="relative flex h-[50vh] w-full flex-col items-center justify-center text-center text-white">
           <div className="relative z-10 mx-4 flex flex-col items-center">
@@ -15,6 +21,8 @@ export default function BlogPage() {
         </section>
         <BlogPosts />
       </main>
+      <WhatsappButton />
+      <Footer />
     </div>
   );
 }
