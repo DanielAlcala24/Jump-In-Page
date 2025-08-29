18baf4270df8ca46cb552e97a04d4abbe0c4b7e7
Dany AV <danielav24102001@gmail.com>
2025-08-29 21:37:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Galeria>
    
    with th

--- src/components/sections/galeria.tsx
+++ src/components/sections/galeria.tsx
@@ -17,7 +17,7 @@ const images = [
 
 export default function Galeria() {
     return (
-        <section id="galeria" className="w-full py-12 bg-white">
+        <section id="galeria" className="w-full py-12 bg-gray-50 dark:bg-gray-900">
             <div className="container mx-auto max-w-7xl px-4 md:px-6">
                 <div className="flex flex-col items-center justify-center space-y-4 text-center">
                     <div className="space-y-2">
