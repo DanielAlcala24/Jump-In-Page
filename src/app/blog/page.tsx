41d5b1e7ec0058530d8f640f1184ae25e8c03b3b
Dany AV <danielav24102001@gmail.com>
2025-08-29 20:34:01 +0000

    en la pagina de blog coloca tambien en la parte de arriba el background

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,13 +3,22 @@ import Footer from '@/components/footer';
 import Header from '@/components/header';
 import SocialIcons from '@/components/social-icons';
 import WhatsappButton from '@/components/whatsapp-button';
+import VideoBackground from '@/components/video-background';
 
 export default function BlogPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
+      <VideoBackground />
       <Header />
       <SocialIcons />
-      <main className="flex-1 pt-24">
+      <main className="flex-1">
+        <section className="relative flex h-96 w-full flex-col items-center justify-center text-center text-white">
+          <div className="relative z-10 mx-4 flex flex-col items-center">
+            <h1 className="text-5xl font-extrabold tracking-tighter text-background sm:text-7xl md:text-8xl font-headline">
+              Nuestro Blog
+            </h1>
+          </div>
+        </section>
         <BlogPosts />
       </main>
       <WhatsappButton />
