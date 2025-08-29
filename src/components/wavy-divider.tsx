1a04e8058540b862799d7ccef42f95ee2450ac56
Dany AV <danielav24102001@gmail.com>
2025-08-29 20:05:38 +0000

    no se ve el divisor

--- src/components/wavy-divider.tsx
+++ src/components/wavy-divider.tsx
@@ -5,29 +5,19 @@ import { cn } from "@/lib/utils";
 export default function WavyDivider() {
     return (
         <div className="bg-white">
-            <div className="relative h-20 w-full overflow-hidden bg-gray-50">
-                <div className="absolute -bottom-px left-0 right-0 h-10 bg-white"
-                     style={{
-                        clipPath: 'path("M0,10 C15,0 35,0 50,10 C65,20 85,20 100,10 V20 H0 Z")'
-                     }}>
-                </div>
-                 <div className="absolute -bottom-px left-0 right-0 h-10 bg-white opacity-50"
-                     style={{
-                        clipPath: 'path("M0,10 C15,20 35,20 50,10 C65,0 85,0 100,10 V20 H0 Z")',
-                        animation: 'wave 2s infinite linear',
-                        animationDelay: '-1s'
-                     }}>
-                </div>
+            <div className="relative h-20 w-full overflow-hidden bg-transparent">
+                <svg
+                    className="absolute bottom-0 left-0 w-full h-auto text-gray-50 dark:text-gray-900"
+                    viewBox="0 0 1440 100"
+                    preserveAspectRatio="none"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M1440 100H0V0c196.67 53.33 493.33 80 890 80c400 0 656.67-26.67 860-80v100z"
+                    ></path>
+                </svg>
             </div>
-            <style jsx>{`
-                @keyframes wave {
-                    0% { transform: translateX(0); }
-                    100% { transform: translateX(-50%); }
-                }
-                div[style*="animation"] {
-                    background-size: 200% auto;
-                }
-            `}</style>
         </div>
     );
 }
